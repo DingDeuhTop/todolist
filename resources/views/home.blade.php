e077fd728d715fbdea096ff0eaa83b8c0ea0a91b
dingliana <dingliana99@gmail.com>
2022-04-09 00:15:12 +0530

    first try

---
+++ resources/views/home.blade.php
@@ -0,0 +1,56 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.11.0/mdb.min.css" rel="stylesheet" />
+    <title>Todo List</title>
+</head>
+
+<body class="bg-blue-400">
+    <h1 class="text-center">welcome</h1>
+    <div class="container w-25 mt-5 m-auto ">
+        <div class="card shadow-sm">
+            <div class="card-body">
+                <h3>To-do List</h3>
+                <form action="{{ route('store') }}" method="post">
+                    @csrf
+                    <div class="input-group">
+                        <input type="text" name="content" class="form-control" placeholder="Enter task" />
+                        <button type="submit" class="btn btn-dark btn-sm px-4">
+                            <i class="fas fa-plus"></i>
+                        </button>
+                    </div>
+                </form>
+                @if (count($todolists))
+                    <ul class="list-group list-group-flush mt-3">
+                        @foreach ($todolists as $todolist)
+                            <li class="list-group-item" style="background-color: #55adad;">
+                                <form action="{{ route('destroy', $todolist->id) }}" method="POST">
+                                    {{ $todolist->content }}
+                                    @csrf
+                                    @method('delete')
+                                    <button type="submit" class="btn btn-link btn-sm float-end" style="color: #fdfdfd;">
+                                        <i class="fas fa-trash"></i>
+                                    </button>
+                                </form>
+                            </li>
+                        @endforeach
+                    </ul>
+                @else
+                    <p class="text-center mt-3">No Tasks</p>
+                @endif
+            </div>
+            @if (count($todolists))
+                <div class="card-footer">
+                    You have {{ count($todolists) }} Pending tasks.
+                </div>
+            @endif
+        </div>
+    </div>
+</body>
+
+</html>
